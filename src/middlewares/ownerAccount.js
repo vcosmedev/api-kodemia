2a3e68d2b2742a08a312dd12b8013f760674dc7b
Victor <victorcosmema@gmail.com>
2022-10-17 01:33:47 -0500

    Add role example Kommunity class

---
+++ src/middlewares/ownerAccount.js
@@ -0,0 +1,1 @@
+// Validar si es la cuenta de un usuario
--- src/routers/koders.router.js
+++ src/routers/koders.router.js
@@ -1,11 +1,31 @@
 import express from 'express'
 import * as kodersUsesCases from '../useCases/koders.use.js'
 import { auth } from '../middlewares/auth.js'
+import {StatusHttp} from '../libs/statusHttp.js'
+
 
 const router = express.Router() // Generar router
 // La comunicación va de fuera hacia dentro
 // Endpoint -> Casos de uso -> Modelos
 
+// ROLE: ADMIN
+router.post('/', async(request, response, next) => {
+    try {
+        const {body: newKoder} = request
+        await kodersUsesCases.create(newKoder)
+
+        response.json({
+            success: true,
+            message: 'Koder creado!'
+        })
+    } catch (error) {
+        // TODO: reemplazar por el middleware del handleErrors
+        next(new StatusHttp(error.message, error.status, error.name))
+    }
+})
+
+
+// ROLE: ADMIN Y USER
 router.get('/', auth, async (request, response, next) => {
     try {
         const allKoders = await kodersUsesCases.getAll()
@@ -26,36 +46,84 @@ router.get('/', auth, async (request, response, next) => {
     }
 })
 
-
-// GET
-
-router.get('/:idKoder', auth, async (request, response) => {
+// ROLE: ADMIN Y USER
+router.get('/', auth, async (request, response, next) => {
     try {
-        const { idKoder } = request.params
-        console.log(idKoder)
-        const getKoder = await kodersUsesCases.getById(idKoder)
+        const allKoders = await kodersUsesCases.getAll()
+        response.json({
+            success: true,
+            data: {
+                koders: allKoders
+            }
+        })
+    } catch (error) {
+        // TODO: reemplazar por el middleware del handleErrors
+        next(new StatusHttp(error.message, error.status, error.name))
+    }
+})
+
+// ROLE: ADMIN Y USER
+router.get('/:id', auth, async (request, response, next) => {
+    try {
+        const {id} = request.params
+        const koder = kodersUsesCases.getById(id)
 
         response.json({
             success: true,
             data: {
-                koders: getKoder
+                koder
             }
         })
     } catch (error) {
-        // Reemplazar por el Middleware del handleErrors
-        response.status(400)
+        // TODO: reemplazar por el middleware del handleErrors
+        next(error)
+    }
+})
+
+// ROLE: ADMIN Y USER
+router.patch('/:id',auth,  async (request, response, next) => {
+    try {
+        const {id} = request.params
+        const {body} = request
+        const koderUpdated = kodersUsesCases.updateById(id,body)
+
         response.json({
-            sucess: false,
-            message: error.message
+            success: true,
+            data: {
+                koder: koderUpdated
+            }
         })
+    } catch (error) {
+        // TODO: reemplazar por el middleware del handleErrors
+        // response.status(400)
+        // response.json({
+        //     success: false,
+        //     message: error.message
+        // })
+        next(new StatusHttp(error.message, error.status, error.name))
+    }
+})
+
+// ROLE: ADMIN Y USER
+router.delete('/:id',auth,  async(request, response, next) => {
+    try {
+        const {id} = request.params
+        await kodersUsesCases.deleteById(id)
+
+        response.json({
+            success: true,
+            message: 'Koder eliminado'
+        })
+    } catch (error) {
+        // PENDING: reemplazar por el middleware del handleErrors
+        next(new StatusHttp(error.message, error.status, error.name))
     }
 })
 
 
 export default router
 
-/*
-15mins - Time to practice:
+/* 15mins - Time to practice:
 
 GET     /koders/:id -> Annie
 PATCH   /koders/:id -> Héctor
