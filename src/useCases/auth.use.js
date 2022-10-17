d14a182a5478767acc441ae8a3a6b56aa5ad114a
Victor <victorcosmema@gmail.com>
2022-10-17 03:09:29 -0500

    Add auth use case

--- src/useCases/auth.use.js
+++ src/useCases/auth.use.js
@@ -1,14 +1,19 @@
 import {Koder} from '../models/koders.model.js'
+import {User} from '../models/user.model.js'
 import bcrypt from '../libs/bcrypt.js'
+import jwt from '../libs/jwt.js'
+import {StatusHttp} from '../libs/statusHttp.js'
 
 async function login(email, password) {
-    const koderFound = await Koder.findOne({email});
+    const koderFound = await Koder.findOne({email})
 
-    if(!koderFound) throw new Error('Credenciales inválidas');
+    // if(!koderFound) throw new Error('Credenciales inválidas')
+    if(!koderFound) throw new StatusHttp('Credenciales inválidas', 400)
 
-    const isValidPassword = bcrypt.compare(password, koderFound.password) // Es una promesa, 'await'
+    const isValidPassword = await bcrypt.compare(password, koderFound.password) // Es una promesa, 'await'
 
-    if(!isValidPassword) throw new Erro('Credenciales inválidas')
+    // if(!isValidPassword) throw new Error('Credenciales inválidas')
+    if(!isValidPassword) throw new StatusHttp('Credenciales inválidas', 400)
 
     return jwt.sign({id: koderFound._id}) // Aquí debe regresar un TOKEN
 }
@@ -16,19 +21,19 @@ async function login(email, password) {
 async function loginUser(email, password) {
     const userFound = await User.findOne({email});
 
-    if(!userFound) throw new Error('Credenciales inválidas');
+    // if(!userFound) throw new Error('Credenciales inválidas');
+    if(!userFound) throw new StatusHttp('Credenciales inválidas', 400);
 
-    const isValidPassword = bcrypt.compare(password, userFound.password) // Es una promesa, 'await'
+    const isValidPassword = await bcrypt.compare(password, userFound.password) // Es una promesa, 'await'
 
-    if(!isValidPassword) throw new Erro('Credenciales inválidas')
+    // if(!isValidPassword) throw new Error('Credenciales inválidas')
+    if(!isValidPassword) throw new StatusHttp('Credenciales inválidas', 400)
 
-    // payload
-    return jwt.sign({id: userFound._id}) // Aquí debe regresar un TOKEN
+    // Pasando el payload
+    return jwt.sign({id: userFound._id, role: userFound.role}) // Aquí debe regresar un TOKEN
 }
 
-
-
-
-
-export{login}
-
+export {
+    login,
+    loginUser
+}
