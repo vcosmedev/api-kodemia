f75aeb1eb00445db78814f346c5b278ab9191288
Victor <victorcosmema@gmail.com>
2022-10-17 03:23:49 -0500

    Add users use case

--- src/useCases/users.use.js
+++ src/useCases/users.use.js
@@ -1,8 +1,8 @@
 import {User} from '../models/user.model.js'
 import bcrypt from '../libs/bcrypt.js'
-import {StatusHttp} from '../libs/statusHttp.js' // Crear carpeta StatusHttp
+import {StatusHttp} from '../libs/statusHttp.js'
 
-async function create () {
+async function create(newUser) {
     // Modificar función 
     const {email, password} = newUser
     // find ({}) -> []
@@ -10,12 +10,15 @@ async function create () {
     // Si encuentra el user, findOne devuelve objeto con el Koder ->  {}
     // Si no encuentra el user, findOne devuelve indefinido -> undefined
 
-    if(userFound) throw new Error('Ya existe un usuario con este email')
-    
+    // if(userFound) throw new Error('Ya existe un usuario con este email')
+    if(userFound) throw new StatusHttp('Ya existe un usuario con este email', 400)
+
     // Encriptar password
-    const encriptedPassword = await bcrypt.hash(password) // Es asíncriono, devuleve promesa, colocar 'await
+    const encriptedPassword = await bcrypt.hash(password) // Es asíncriono, devuelve promesa, colocar 'await'
 
     return User.create({...newUser, password: encriptedPassword})
 }
 
-
+export {
+    create
+}
