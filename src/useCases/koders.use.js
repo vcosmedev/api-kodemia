a7f411835fc0833aca0fc5893b0998840c06423b
Victor <victorcosmema@gmail.com>
2022-10-17 03:20:54 -0500

    Add koder use case

--- src/useCases/koders.use.js
+++ src/useCases/koders.use.js
@@ -1,9 +1,6 @@
 import {Koder} from '../models/koders.model.js'
-import bcryt from '../libs/bcrypt.js'
-
-function  getAll() {
-    return Koder.find({}) // Regresa la promsea
-}
+import bcrypt from '../libs/bcrypt.js'
+import { StatusHttp } from '../libs/statusHttp.js'
 
 async function create(newKoder) {
     // Modificar función 
@@ -13,25 +10,35 @@ async function create(newKoder) {
     // Si encuentra el user, findOne devuelve objeto con el Koder ->  {}
     // Si no encuentra el user, findOne devuelve indefinido -> undefined
 
-    if(koderFound) throw new Erro('Ya existe un Koder con este email')
+    // if(koderFound) throw new Error('Ya existe un Koder con este email')
+    if(koderFound) throw new StatusHttp('Ya existe un Koder con este email', 400)
     
     // Encriptar password
-    const encriptedPassword = await bcrypt.hash(password) // Es asíncriono, devuleve promesa, colocar 'await
+    const encriptedPassword = await bcrypt.hash(password) // Es asíncriono, devuleve promesa, colocar 'await'
 
     return Koder.create({...newKoder, password: encriptedPassword})
 }
 
-
-// Diferencia entre export y export default
+function getAll() {
+    return Koder.find({}) // Regresa la promesa
+}
 
 function getById(idKoder) {
-
     return Koder.findById(idKoder)
 }
 
-export {
-    getById,
-    getAll
+function updatedById(idKoder, newData) {
+    return Koder.findByIdAndUpdate(idKoder, newData, {new: true}) // -> new: true****
 }
 
+function deleteById(idKoder) {
+    return Koder.findOneAndDelete(idKoder)
+}
 
+export {
+    create,
+    getAll,
+    getById,
+    updatedById,
+    deleteById
+}
