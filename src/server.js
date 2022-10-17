033c0c6f18f463b75a1ae074c3d5e9011ca0cd95
Victor <victorcosmema@gmail.com>
2022-10-17 03:27:15 -0500

    Add changes to server archive

--- src/server.js
+++ src/server.js
@@ -3,19 +3,21 @@ server:
     Este archivo guardará la definición del server:
         - Qué middlewares se van a usar - montar middlewares
         - Montar routers
+
+Definición del servidor (aquí NO se levanta, solo se establecen la configuración)
 */
 
-// Definición de nuestro servidor (aquí no se levanta, solo se establecen la configuración)
-
 import express from 'express'
+import cors from 'cors' // 
 import kodersRouter from './routers/koders.router.js'
 import usersRouter from './routers/user.router.js'
 import authRouter from './routers/auth.router.js'
-
+import {errorHandle} from './middlewares/errorHandle.js'
 const server = express()
 
 // Middlewares
 server.use(express.json())
+server.use(cors())
 
 // Routers
 server.use('/koders', kodersRouter)
@@ -23,7 +25,7 @@ server.use('auth', authRouter)
 server.use('/users', usersRouter)
 
 // Middleware para manejar errores - Middleware handleErros
-
+server.use(errorHandle)
 
 // Exportar este servidor para que pueda ser usado en el index.js
 export {server}
